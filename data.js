59b70213b33d32dc0c265334654fe54f142dbe7f
Phạm Phú Quý Đôn <donezombie@gmail.com>
2019-07-06 17:26:34 +0700

    .

---
+++ data.js
@@ -0,0 +1,55 @@
+const data = {
+  alias: "DZB",
+  quotes: "'' Cặc ''",
+  authorQuotes: "~ Phạm Phú Quý Đôn",
+  summary: "Hi Guys, I'm Pham Phu Quy Don a.k.a Donezombie or DZB <br/> I hold a position as Web Front-end Developer.",
+  mySkill: [
+    {
+      name: 'React',
+      src: 'static/logoSkill/react.png'
+    },
+    {
+      name: 'Python',
+      src: 'static/logoSkill/python.png'
+    },
+    {
+      name: 'Javascript',
+      src: 'static/logoSkill/js.png'
+    },
+    {
+      name: 'HTML',
+      src: 'static/logoSkill/html.png'
+    },
+    {
+      name: 'CSS',
+      src: 'static/logoSkill/css.png'
+    },
+  ],
+  myProject: [
+    {
+      name: 'Techkids Learning Management System',
+      src: 'static/project/learningsystem.png',
+      url: 'https://apps.techkids.vn/',
+    },
+    {
+      name: 'MindX FAQ',
+      src: 'https://mindx.edu.vn/static/images/logo.png',
+      url: 'https://faq.mindx.edu.vn/',
+    },
+    {
+      name: 'Hangman Game',
+      src: 'static/project/hangman.png',
+      url: 'https://donezombie.github.io/hangman/',
+    },
+    {
+      name: 'Cheating Vote Web (useLess)',
+      src: 'static/project/cheating.png',
+      url: 'https://donezombie.github.io/uselessWeb/',
+    },
+    {
+      name: 'Vietnamese Lungs (greate legacy)',
+      src:  'https://res.cloudinary.com/teepublic/image/private/s--q7G6a19F--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1526978219/production/designs/2712635_0.jpg',
+      url: 'https://laphoiviet.herokuapp.com/'
+    },
+  ]
+}
---
+++ main.js
@@ -0,0 +1,54 @@
+const alias = document.getElementById('title').children[0];
+const quotes = document.getElementById('top').children[1].children[0];
+const authorQuotes = document.getElementById('top').children[1].children[1];
+const skillWrapper = document.getElementById('left').children[1];
+const projectWrapper = document.getElementById('right').children[1];
+const summary = document.getElementById('bottom-summary').children[0];
+// data var import from data.js
+
+
+quotes.innerHTML = data.quotes;
+authorQuotes.innerHTML = data.authorQuotes
+alias.innerHTML = data.alias;
+summary.innerHTML = data.summary;
+
+const mySkill = data.mySkill;
+const myProject = data.myProject;
+
+const clickToVisit = (e) => {
+  const url = e.getAttribute('url');
+  window.open(url);
+}
+
+mySkill.forEach(el => {
+  let HTML = `
+  <div class="item wow bounceInLeft">
+    <img src=${el.src} alt="logo" />
+    <div class="tip">
+      ${el.name}
+    </div>
+  </div>`
+  skillWrapper.insertAdjacentHTML('beforeend', HTML);
+});
+
+myProject.forEach(el => {
+  let HTML = `
+    <div url="${el.url}" class="project wow bounceInRight" onclick="clickToVisit(this)" >
+      <div class="project-content">
+        <img src="${el.src}" />
+      </div>
+      <div class="project-title">
+        <div style=" flex: 100%; color: #c4b998"><p>${el.name}</p></div>
+        <div><p>Click to visit page >>> </p></div>
+        <div class="project-icon">
+          <img style="object-fit: cover; width: 100%" src="static/icon_h4.png" />
+        </div>
+      </div>
+    </div>
+  `;
+  projectWrapper.insertAdjacentHTML('beforeend', HTML);
+});
+
+document.getElementById('avatar').addEventListener('click', () => {
+  window.open('https://fb.com/donezombiee');
+})
